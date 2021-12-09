087edb4a31010aa5c6fa317a7d92628f98326e07
glinda93 <72871001+glinda93@users.noreply.github.com>
2021-12-10 04:54:50 +0900

    Add scripts

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -4,68 +4,90 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract SimpleEthereumWallet {
+    using SafeMath for uint256;
 
-  using SafeMath for uint256;
+    address owner;
+    mapping(address => User) public users;
 
-  address owner;
-  mapping(address => User) public users;
-
-  struct User {
-    bytes32 passwordHash;
-    uint256 balance;
-  }
-
-  modifier onlyOwner() {
-    require(msg.sender == owner, "Not the owner");
-    _;
-  }
-
-  modifier registerPassword(bytes32 password) {
-    if (users[msg.sender].passwordHash != 0) {
-      _;
-    } else {
-      users[msg.sender].passwordHash = keccak256(abi.encodePacked(password, msg.sender));
-      _;
+    struct User {
+        bytes32 passwordHash;
+        uint256 balance;
     }
-  }
 
-  modifier checkPassword(bytes32 password) {
-    require(users[msg.sender].passwordHash == keccak256(abi.encodePacked(password, msg.sender)), "Password does not match");
-    _;
-  }
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the owner");
+        _;
+    }
 
-  modifier checkBalance(uint256 amount) {
-    require(users[msg.sender].balance > amount, "Insufficient balance");
-    _;
-  }
+    modifier registerPassword(bytes32 password) {
+        if (users[msg.sender].passwordHash != 0) {
+            _;
+        } else {
+            users[msg.sender].passwordHash = keccak256(
+                abi.encodePacked(password, msg.sender)
+            );
+            _;
+        }
+    }
 
-  constructor(bytes32 password) payable registerPassword(password) {
-    owner = msg.sender;
-    users[msg.sender].balance = msg.value;
-  }
+    modifier checkPassword(bytes32 password) {
+        require(
+            users[msg.sender].passwordHash ==
+                keccak256(abi.encodePacked(password, msg.sender)),
+            "Password does not match"
+        );
+        _;
+    }
 
-  function deposit(bytes32 password) external payable registerPassword(password) checkPassword(password) {
-    users[msg.sender].balance = msg.value;
-  }
+    modifier checkBalance(uint256 amount) {
+        require(users[msg.sender].balance > amount, "Insufficient balance");
+        _;
+    }
 
-  function withdraw(bytes32 password, uint256 amount) external checkPassword(password) checkBalance(amount) {
-    users[msg.sender].balance -= amount;
-    (bool success, ) = payable(msg.sender).call{value: amount}("");
-    require(success, "Fail to send");
-  }
+    constructor(bytes32 password) payable registerPassword(password) {
+        owner = msg.sender;
+        users[msg.sender].balance = msg.value;
+    }
 
-  function transfer(bytes32 password, address payee, uint256 amount) external checkPassword(password) checkBalance(amount) {
-    require(msg.sender != payee, "Cannot transfer to self");
-    require(users[payee].passwordHash != 0, "Payee does not have password");
-    users[msg.sender].balance -= amount;
-    users[payee].balance += amount;
-  }
+    function deposit(bytes32 password)
+        external
+        payable
+        registerPassword(password)
+        checkPassword(password)
+    {
+        users[msg.sender].balance = msg.value;
+    }
 
-  function getBalance(address addr) external view returns(uint256) {
-    return users[addr].balance;
-  }
+    function withdraw(bytes32 password, uint256 amount)
+        external
+        checkPassword(password)
+        checkBalance(amount)
+    {
+        users[msg.sender].balance -= amount;
+        (bool success, ) = payable(msg.sender).call{value: amount}("");
+        require(success, "Fail to send");
+    }
 
-  function closeService(bytes32 password) external onlyOwner checkPassword(password) {
-    selfdestruct(payable(owner));
-  }
+    function transfer(
+        bytes32 password,
+        address payee,
+        uint256 amount
+    ) external checkPassword(password) checkBalance(amount) {
+        require(msg.sender != payee, "Cannot transfer to self");
+        require(users[payee].passwordHash != 0, "Payee does not have password");
+        users[msg.sender].balance -= amount;
+        users[payee].balance += amount;
+    }
+
+    function getBalance(address addr) external view returns (uint256) {
+        return users[addr].balance;
+    }
+
+    function closeService(bytes32 password)
+        external
+        onlyOwner
+        checkPassword(password)
+    {
+        selfdestruct(payable(owner));
+    }
 }
